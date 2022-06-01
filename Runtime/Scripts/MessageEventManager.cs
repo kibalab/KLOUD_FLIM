59e238642fdc52c2bded5c2bfc3a6d4c81fcea25
KIBA <kjh030529@gmail.com>
2022-06-01 10:38:09 +0900

    Create MessageEvent

--- Runtime/Scripts/MessageEventManager.cs
+++ Runtime/Scripts/MessageEventManager.cs
@@ -1,7 +1,33 @@
+using System.Collections.Generic;
+
 namespace KLOUD
 {
-    public class MessageEventManager
+    public class MessageEvent
     {
-        
+        public string Message;
+
+        public MessageEvent(string message)
+        {
+            Message = message;
+        }
+    }
+    
+    public static class MessageEventManager
+    {
+        public static Queue<MessageEvent> Events = new Queue<MessageEvent>();
+
+        public static void Enqueue(string msg)
+        {
+            Events.Enqueue(new MessageEvent(msg));
+        }
+
+        public static MessageEvent Dequeue()
+        {
+            if (IsEmpty()) return null;
+
+            return Events.Dequeue();
+        }
+
+        public static bool IsEmpty() => Events.Count == 0;
     }
 }
